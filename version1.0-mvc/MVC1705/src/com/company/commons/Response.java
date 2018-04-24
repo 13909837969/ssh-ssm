24aa3d16a5fc4bbba311ddcb2a3bf1129d47050b
wuwentao <1985650581@qq.com>
2018-04-24 13:42:34 +0800

    eclipse 优化

--- version1.0-mvc/MVC1705/src/com/company/commons/Response.java
+++
@@ -1,6 +0,0 @@
-package com.company.commons;
-
-public class Response {
-	public static final String SUCCESS = "success";
-	public static final String ERROR = "error";
-}
--- version3.0-hibernate+struts2/MVC1705/src/com/company/service/iservice/IDeptService.java
+++
@@ -1,7 +0,0 @@
-package com.company.service.iservice;
-
-import com.company.dao.pojo.Dept;
-
-public interface IDeptService extends IBaseService<Dept,Integer> {
-
-}
